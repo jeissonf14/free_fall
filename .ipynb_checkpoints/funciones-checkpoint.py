1ddfb7e425ff515e8be9190f5456f47e298fb433
apreziosir <apreziosir@unal.edu.co>
2020-09-04 21:11:17 -0500

    200904 Initial commit with original code working

---
+++ .ipynb_checkpoints/funciones-checkpoint.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+# Funcion que calcula el coeficiente de arrastre de una esfera en caida libre
+# Funciona con Reynolds descde 0 hasta m'as all'a de 3e6
+def Cd(u, D, nu):
+    
+    # Calculando el numero de Reynolds
+    Re = u * D / nu
+    
+    # Definiendo el Coeficiente de arrastr C_d
+    if Re == 0: 
+        
+        Cd = 0
+    
+    elif np.logical_and(Re > 0, Re <= 1):
+        
+        Cd = 24 / Re
+        
+    elif np.logical_and(Re > 1, Re <= 400):
+        
+        Cd = 24 / Re ** 0.646
+        
+    elif np.logical_and(Re > 400, Re <= 3e5):
+        
+        Cd = 0.5
+        
+    elif np.logical_and(Re > 3e5, Re <= 2e6):
+        
+        Cd = 0.000366 * Re ** 0.4275
+        
+    else: 
+        
+        Cd = 0.18
+        
+    return Cd
+
+# Funcion para calcular el paso incremental de RK4. 
+# Funciona con el codigo de caida libre
+def Fi(A, B, C, CD, x):
+   
+    vsq = x ** 2
+    Fi = (1 / A) * (B - C * vsq * CD)
+    
+    return Fi
